81feb41df5b2f74cb8b531908d0d71333b6a6559
Marlon Rosa <marlonrosa@MacBook-Air-de-Marlon.local>
2024-12-20 01:25:35 -0300

    learn Single/Doubly LinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class MyLinkedList {
   public static void main(String[] args) {
@@ -21,12 +21,50 @@ public class MyLinkedList {
     //              2. Insertion and deletation of nodes is easy. O(1)
     //              3. No/Low memory waste
 
-    //
+    //              disadvantages?
+    //              1. Greater memory usage (additional pointer)
+    //              2. No random access of elements (no index [i])
+    //              3. Accessing/searching elements is more time consuming. 0(n)
+    
+    //              uses?
+    //              1. implement Stacks/Queues
+    //              2. GPS navigation
+    //              3. Music playlist 
       
     //  **********************************************
 
     LinkedList<String> linkedlist = new LinkedList<String>();
 
+    /*
+    linkedlist.push("A");
+    linkedlist.push("B");
+    linkedlist.push("C");      //messy order 
+    linkedlist.push("D");
+    linkedlist.push("E");
+    linkedlist.pop();
+    */
+    
+    linkedlist.offer("A");
+    linkedlist.offer("B");
+    linkedlist.offer("C");
+    linkedlist.offer("D");
+    linkedlist.offer("E");
+    //linkedlist.poll();
+    
+    linkedlist.add(3, "f");
+    linkedlist.remove("f");
+
+   //System.out.println(linkedlist.indexOf("A"));
+    System.out.println(linkedlist.peekFirst());
+    System.out.println(linkedlist.peekLast());
+    linkedlist.addFirst("0");
+    linkedlist.addLast("G");
+    String first = linkedlist.removeFirst();
+    String last = linkedlist.removeLast();
+  
+
+    System.out.println(linkedlist);
 
   }
-}
+
+
